6c9722615b68bf209c930befb118e02fd1a0698e
EduardoA3677 <82284031+EduardoA3677@users.noreply.github.com>
2021-09-03 18:11:45 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@ TARGET_OTA_ASSERT_DEVICE := vayu,bhima
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := kryo485
